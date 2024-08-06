a4c7c9cbecae3ddbac05a212e9647683d12a0079
kwyoon0705 <kwyoon0705@gmail.com>
2024-08-06 16:01:39 +0900

    day16 commit

---
+++ day-16-OOP/main.py
@@ -0,0 +1,23 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+
+ongoing = True
+
+while ongoing:
+    options = menu.get_items()
+    order = input(f"What would you like? ({options}) : ")
+    if order == "off":
+        ongoing = False
+    elif order == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(order)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
